eab9644fd1b0bf8601768f8ae6758bbc3d08e2d1
Felipe Mendes <felipemadruga@gmail.com>
2023-03-09 14:52:42 -0300

    Configura redux

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+const wallet = (state = INITIAL_STATE, actions) => {
+  switch (actions.type) {
+  default: return state;
+  }
+};
+
+export default wallet;
