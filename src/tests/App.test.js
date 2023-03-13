afa3618930d7d80cd713673892d52f5b0f660b74
Felipe Mendes <felipemadruga@gmail.com>
2023-03-13 18:37:44 -0300

    Inicia implementação de testes

---
+++ src/tests/App.test.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+
+describe('Testa página de login', () => {
+  test('Está presente o input de senha e de email', () => {
+    renderWithRouterAndRedux(<App />);
+
+    expect(screen.getAllByRole('textbox')).toHaveLength(2);
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/senha/i)).toBeInTheDocument();
+  });
+
+  test('É possivel escrever nos inputs de email e senha', () => {
+    renderWithRouterAndRedux(<App />);
+    const emailTest = 'testando_input_de_email';
+    const senhaTest = '123456';
+
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputSenha = screen.getByPlaceholderText(/senha/i);
+
+    userEvent.type(inputEmail, emailTest);
+    expect(inputEmail.value).toBe('testando_input_de_email');
+
+    userEvent.type(inputSenha, senhaTest);
+    expect(inputSenha.value).toBe('123456');
+  });
+
+  test('O botão entrar é renderizado e está desabilitado', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const btnEntrar = screen.getByRole('button');
+    expect(btnEntrar).toBeDisabled();
+  });
+
+  test('O botão só é habilitado qd as condições dos inouts são alcançadas', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputSenha = screen.getByPlaceholderText(/senha/i);
+    const btnEntrar = screen.getByRole('button');
+
+    userEvent.type(inputEmail, 'felipe@gmail.com');
+    userEvent.type(inputSenha, '123456');
+
+    expect(btnEntrar).toBeEnabled();
+  });
+
+  test('O botão continua desabiltiado se um dos inputs não atender às condições', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputSenha = screen.getByPlaceholderText(/senha/i);
+    const btnEntrar = screen.getByRole('button');
+
+    userEvent.type(inputEmail, 'felipe@gmail.c');
+    userEvent.type(inputSenha, '12345');
+
+    expect(btnEntrar).toBeDisabled();
+  });
+
+  test('Ao clicar no botão, leva a rota /carteira', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputSenha = screen.getByPlaceholderText(/senha/i);
+    const btnEntrar = screen.getByRole('button');
+
+    userEvent.type(inputEmail, 'felipe@gmail.com');
+    userEvent.type(inputSenha, '123456');
+    expect(btnEntrar).toBeEnabled();
+
+    userEvent.click(btnEntrar);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/carteira');
+  });
+});
+
+describe('Testa componente Header', () => {
+  test('O Header é renderizado com o email logado, total 0 e moeda BRL', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const inputEmail = screen.getByPlaceholderText(/email/i);
+    const inputSenha = screen.getByPlaceholderText(/senha/i);
+    const btnEntrar = screen.getByRole('button');
+
+    userEvent.type(inputEmail, 'felipe@gmail.com');
+    userEvent.type(inputSenha, '123456');
+    expect(btnEntrar).toBeEnabled();
+
+    userEvent.click(btnEntrar);
+
+    expect(history.location.pathname).toBe('/carteira');
+
+    expect(screen.getByText(/Email: felipe@gmail.com/i)).toBeInTheDocument();
+    expect(screen.getByText(0)).toBeInTheDocument();
+    expect(screen.getByText('BRL')).toBeInTheDocument();
+  });
+});
+
+describe('Testa component WalletForm', () => {
+  test('Renderiza todos os components do formulario', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    act(() => {
+      history.push('/carteira');
+    });
+    expect(history.location.pathname).toBe('/carteira');
+
+    const valorInput = screen.getByLabelText('Valor:');
+    const descricaoInput = screen.getByLabelText('Descrição:');
+    const moedaInput = screen.getByLabelText('Moeda:');
+    const methodInput = screen.getByLabelText('Método de Pagamento');
+    const categoriaInput = screen.getByLabelText('Categoria:');
+    const btnAdd = screen.getByRole('button', { name: 'Adicionar despesa' });
+
+    expect(valorInput).toBeInTheDocument();
+    expect(descricaoInput).toBeInTheDocument();
+    expect(moedaInput).toBeInTheDocument();
+    expect(methodInput).toBeInTheDocument();
+    expect(categoriaInput).toBeInTheDocument();
+    expect(btnAdd).toBeInTheDocument();
+  });
+
+  test('Os valores preenchidos são enviados para o estado global', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    act(() => {
+      history.push('/carteira');
+    });
+    expect(history.location.pathname).toBe('/carteira');
+    screen.logTestingPlaygroundURL();
+
+    const valorInput = screen.getByLabelText('Valor:');
+    const descricaoInput = screen.getByLabelText('Descrição:');
+    const moedaInput = screen.getByRole('combobox', { name: /moeda:/i });
+    const methodInput = screen.getByLabelText('Método de Pagamento');
+    const categoriaInput = screen.getByLabelText('Categoria:');
+    const btnAdd = screen.getByRole('button', { name: 'Adicionar despesa' });
+    console.log(moedaInput);
+
+    userEvent.type(valorInput, '10');
+    userEvent.type(descricaoInput, 'gasto 01');
+    userEvent.selectOptions(moedaInput, 'EUR');
+    // TENTANDO SELECIONAR A OPÇÃO DO SELECT, MAS SE NAÕ CONSEGUIR LOGO, DEIXA COMO ESTÁ NO PADRÃO
+
+    expect(screen.getByText('EUR')).toBeVisible();
+  });
+});
